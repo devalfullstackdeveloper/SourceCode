0a4501018bbbdbc6f5490e1aa8bde4ce9785d60b
devalfullstackdeveloper <devalfullstackdeveloper@gmail.com>
2019-08-09 20:24:04 +0530

    account and marker page changes

--- config/database.js
+++ config/database.js
@@ -50,12 +50,12 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', '127.0.0.1'),
+      host: Env.get('DB_HOST', '142.93.231.155'),
       port: Env.get('DB_PORT', '3306'),
       //localhost
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', 'saurabh'),
-      database: Env.get('DB_DATABASE', 'CryptoFxSpaceDb'),
+      user: Env.get('DB_USER', 'zithex_prod'),
+      password: Env.get('DB_PASSWORD', 'ZithexProd@2019'),
+      database: Env.get('DB_DATABASE', 'zithex_prod'),
       //design
       // user: Env.get('DB_USER', 'zithex19_design'),
       // password: Env.get('DB_PASSWORD', 'Zithex@2019'),
